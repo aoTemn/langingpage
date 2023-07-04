e9e9c39c4a37f92c3328f738638faf1608b60216
czc <1073138985@qq.com>
2023-07-04 20:01:41 +0800

    提交

---
+++ components/common/Footer.vue
@@ -0,0 +1,120 @@
+<script setup >
+import { ref, onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps({})
+onMounted(() => { })
+let list = computed(() => {
+  return [
+    {
+      title: 'Product',
+      linkList: [
+        {
+          title: 'Product',
+          link: '',
+        },
+        {
+          title: 'Product',
+          link: '',
+        },
+        {
+          title: 'Product',
+          link: '',
+        }
+      ]
+    },
+    {
+      title: 'Feature',
+      linkList: [
+        {
+          title: 'Feature',
+          link: '',
+        },
+        {
+          title: 'Feature',
+          link: '',
+        },
+        {
+          title: 'Feature',
+          link: '',
+        }
+      ]
+    },
+    {
+      title: 'Resource',
+      linkList: [
+        {
+          title: 'Resource',
+          link: '',
+        },
+        {
+          title: 'Resource',
+          link: '',
+        },
+        {
+          title: 'Resource',
+          link: '',
+        }
+      ]
+    },
+    {
+      title: 'Company',
+      linkList: [
+        {
+          title: 'Company',
+          link: '',
+        },
+        {
+          title: 'Company',
+          link: '',
+        },
+        {
+          title: 'Company',
+          link: '',
+        }
+      ]
+    },
+  ]
+})
+</script>
+<template>
+  <div class='bg-white relative'>
+    <div
+      class="absolute left-1/2 -top-[100px] -translate-x-1/2 flex flex-col items-center w-4/5 rounded-3xl xl:w-[1345px] px-8 xl:px-0 py-20 bg-[url('/img/footer-thumb.png')] bg-[length:100%_100%]">
+      <h3 class="text-3xl xl:text-[48px] text-white">Let's talk</h3>
+      <p class="mt-5 text-sm xl:text-base text-center text-slate-400 xl:w-[575px]">
+        Our product specialists and customer success team are always available to help you write your content. Feel free
+        to contact us for any queries.
+      </p>
+      <button class="mt-12 w-[168px] h-[56px] bg-gradient-to-b from-[#1ABAED] to-[#0073DC] text-white rounded">Contact Us
+      </button>
+    </div>
+    <div
+      class="flex flex-col items-center xl:items-stretch  xl:flex-row justify-center gap-10 xl:gap-[270px] bg-[#061237] rounded-t-3xl xl:rounded-t-[60px] pt-[363px] pb-[110px]">
+      <img class="w-[140px] h-[60px]" src="/img/logo.png" alt="">
+      <div class="flex flex-wrap px-6 xl:px-0 gap-y-10 xl:gap-40 ">
+        <div class="flex flex-col  gap-6 text-white w-[160px] pl-4 xl:w-auto" v-for="item in list" :key="item.title">
+          <h5 class="text-xl">{{ item.title }}</h5>
+          <div class="flex flex-col gap-4 ">
+            <a class="cursor-pointer" v-for="(link, i) in item.linkList" :key="i">{{ link.title }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-[#061237] xl:hidden flex gap-4 text-[#FFFFFFB2] text-sm xl:text-base justify-end px-6 pb-4">
+      <a>Privacy</a>
+      <a>Terms</a>
+    </div>
+    <div class="bg-[#061237]">
+      <div
+        class="px-6 xl:px-0 xl:max-w-[1320px] mx-auto h-14 xl:h-20 border-t-2 border-t-line flex items-center justify-center xl:justify-between text-[#FFFFFFB2] text-sm xl:text-base">
+        <p>© 2010-2020 Access Media. All rights reserved.</p>
+        <div class="hidden xl:flex  gap-4 cursor-pointer">
+          <a>Privacy</a>
+          <a>Terms</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<style scoped lang='less'></style>
---
+++ components/home/FormArticle.vue
@@ -0,0 +1,60 @@
+<script setup >
+import { ref, onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps({})
+onMounted(() => { })
+let queryParams = computed(() => { })
+</script>
+<template>
+  <div
+    class='flex flex-col-reverse px-6 xl:px-0 xl:flex-row justify-center gap-[110px] xl:gap-[290px] bg-[#061237] py-[68px] xl:py-[200px]'>
+    <div class="flex flex-col items-center xl:items-start w-full xl:w-[500px]">
+      <input class="w-full rounded-lg h-[56px] bg-[#040c25] shadow-input-box border-[#1ABAED] border-[1px]" type="text">
+      <input class="mt-6 w-full rounded-lg h-[56px] bg-[#040c25] shadow-input-box border-[#1ABAED] border-[1px]"
+        type="text">
+      <input class="mt-6 w-full rounded-lg h-[56px] bg-[#040c25] shadow-input-box border-[#1ABAED] border-[1px]"
+        type="text">
+      <button
+        class="mt-8 w-[168px] h-[56px] bg-gradient-to-b from-[#1ABAED] to-[#0073DC] text-white rounded">Generate</button>
+      <textarea class="mt-16 w-full rounded-lg h-[266px] bg-[#040c25] shadow-input-box border-[#1ABAED] border-[1px]"
+        name="" id="" cols="30" rows="10"></textarea>
+      <button class="mt-8 w-[168px] h-[56px] bg-gradient-to-b from-[#1ABAED] to-[#0073DC] text-white rounded">Start
+        Now</button>
+    </div>
+    <div class="w-full xl:w-[533px] flex flex-col items-center text-center xl:text-left ">
+      <h3 class="text-white w-full font-bold xl:w-[473px] text-3xl xl:text-4xl">
+        Everything need to quickly write exciting new article
+      </h3>
+      <p class="mt-4 text-white text-opacity-80 w-full xl:w-[473px] text-sm xl:text-base">From in-depth tutorials, to
+        detailed
+        changelogs, to
+        comprehensive
+        documentation, AssemblyAI is focused on
+        providing developers a great experience every step of the way.</p>
+      <div class="relative form-article-box mt-[66px] w-[273px] h-[299px] xl:w-[533px] xl:h-[412px] text-left">
+        <div
+          class="absolute form-article-box -top-5 -translate-x-1/4 xl:-translate-x-1/2 w-[193px] xl:w-[350px] p-3 xl:p-6">
+          <h4 class="text-white text-base xl:text-2xl font-semibold">Save Your Time</h4>
+          <p class="mt-[10px] text-white text-xs xl:text-sm">
+            AI content generators can help you scale your content strategy quickly and easily without spending a time.
+          </p>
+        </div>
+        <div
+          class="absolute form-article-box top-[115px] xl:top-[78px] right-0 translate-x-1/4 xl:translate-x-1/3 w-[193px] xl:w-[350px] p-3 xl:p-6 ">
+          <h4 class="text-white text-base xl:text-2xl font-semibold">Unique Article</h4>
+          <p class="mt-[10px] text-white text-xs xl:text-sm">
+            AI content generators can help you scale your content strategy quickly and easily without spending a time.
+          </p>
+        </div>
+        <div class="absolute form-article-box -bottom-16 xl:bottom-5 -translate-x-1/3 w-[193px] xl:w-[350px] p-3 xl:p-6">
+          <h4 class="text-white text-base xl:text-2xl font-semibold">Different Types of Articles</h4>
+          <p class="mt-[10px] text-white text-xs xl:text-sm">
+            There are a number of different types of articles that AI can write foryou.
+          </p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang='less'></style>
